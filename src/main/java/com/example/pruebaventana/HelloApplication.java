62165db2e4fce013dd870f1d6a6ca9aadfae5bcb
Jacqueline0595 <janellejackelinegomezbenitez@gmail.com>
2026-03-26 13:34:18 -0600

    Intento patetico de hacer la ventana

--- src/main/java/com/example/pruebaventana/HelloApplication.java
+++ src/main/java/com/example/pruebaventana/HelloApplication.java
@@ -2,21 +2,54 @@ package com.example.pruebaventana;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
+
+    private static final int PASO = 10;
+
     @Override
     public void start(Stage stage){
+
+        Image image = new Image("/Felix/Felix1.png", 15, 33, false, false);
+        ImageView imageView = new ImageView(image);
+
+        Image img = new Image("/Fondo/BackgroundInitial.png", 240, 255, false, false);
+        ImageView fondo = new ImageView(img);
+
+        imageView.setScaleX(2);
+        imageView.setScaleY(2);
+
+        fondo.setScaleX(2);
+        fondo.setScaleY(2);
+
         Pane root = new Pane();
-        root.setStyle(
-                "-fx-background-image: url('Felix1.png');" +
-                "-fx-background-size: cover;"
-        );
-        Scene scene = new Scene(root, 735, 824);
-        stage.setTitle("Gato");
+
+        root.getChildren().add(fondo);
+        root.getChildren().add(imageView);
+
+        Scene scene = new Scene(root, 240, 255);
+
+        scene.setCamera(null);
+
+        scene.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case W: imageView.setLayoutY(imageView.getLayoutY() - PASO); break;
+                case S: imageView.setLayoutY(imageView.getLayoutY() + PASO); break;
+                case A: imageView.setLayoutX(imageView.getLayoutX() - PASO); break;
+                case D: imageView.setLayoutX(imageView.getLayoutX() + PASO); break;
+                default: break;
+            }
+        });
+
+        stage.setTitle("Felix FixIt");
         stage.setScene(scene);
         stage.show();
-        System.out.println("Hello World!"); // poniendo el ;
+
+        imageView.setFocusTraversable(true);
+        imageView.requestFocus();
     }
 }
